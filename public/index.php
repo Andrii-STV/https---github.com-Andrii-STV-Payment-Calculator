d807b94743d159a999d77b6371964cebce29db3e
Andrii <stavnichenkoa2000@gmail.com>
2023-07-28 13:57:51 +0300

    created Product Class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
 <?php
 
-echo 'check';
+class Product {
+    private $productName;
+    private $productPrice;
+
+    function __construct($productName, $productPrice) {
+        $this->productName = $productName;
+        $this->productPrice = $productPrice;
+    }
+
+    function getProductPrice() {
+        return $this->productPrice;
+    }
+
+    function getProductName() {
+        return $this->productName;
+    }
+}
+
+
+$productsArray = [
+    "Shirt" => 50,
+    "Pants" => 100,
+    "Shoes" => 200
+];
+
+
+
+
+$product1 = new Product (key($productsArray), $productsArray["Shirt"]);
+
+echo $product1->getProductPrice();
