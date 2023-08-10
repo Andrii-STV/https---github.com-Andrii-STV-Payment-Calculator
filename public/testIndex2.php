ce7bf4a2d762af57a1293b983484c464cff64023
Andrii <stavnichenkoa2000@gmail.com>
2023-08-10 20:19:21 +0300

    Checked possible functions to find good approach

--- public/testIndex2.php
+++ public/testIndex2.php
@@ -20,14 +20,27 @@ $newProducts = array_column($productsArray, 'productName');
 $newDiscount = array_column($discountArray, 'productName');
 
 
+
 foreach ($newDiscount as $key => $productName) {                       //count through newDiscount array 
 
 
     if (in_array($productName, $newProducts, true)) {                  //here in $productName values 'Shirt' and 'Pants' are stored which are taken from newDiscount array, but we check newProduct array
+        
+        $calculate;
         echo "discount should be applied<br>";
+
     }
 
 }
+
+function testPrint($item, $key) {
+    echo "$key holds $item<br>";
+}
+
+
+array_walk_recursive($productsArray, 'testPrint');
+
+
 
 echo '<br><hr><br>';
 var_dump($productsArray);
